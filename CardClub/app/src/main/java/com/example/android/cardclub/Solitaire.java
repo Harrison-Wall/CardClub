63dbd4b4fe8386b87f338c3dd0648902f2c1183f
Harrison Wall <harrisonwall99@yahoo.com>
2018-11-04 19:34:17 -0500

    Added Solitaire Class

---
+++ CardClub/app/src/main/java/com/example/android/cardclub/Solitaire.java
@@ -0,0 +1,19 @@
+package com.example.android.cardclub;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Gallery;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+public class Solitaire extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstancestate)
+    {
+        super.onCreate(savedInstancestate);
+
+
+    }
+}
