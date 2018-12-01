853ce1b1c4bbb37a5c6c27057348936f8c4a5e0e
Harrison Wall <harrisonwall99@yahoo.com>
2018-12-01 16:08:18 -0500

    Initialization done

--- CardClub/app/src/main/java/com/example/android/cardclub/BlackJack.java
+++ CardClub/app/src/main/java/com/example/android/cardclub/BlackJack.java
@@ -23,6 +23,7 @@ public class BlackJack extends Activity
     private int marginOffset;
 
     private TextView userScoreView, dealerScoreView;
+    private ViewGroup.LayoutParams myparams;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -65,30 +66,43 @@ public class BlackJack extends Activity
         dealerScore = 0;
         dealerCards = new Vector<Card>();
 
+        //Set up textviews
+        dealerScoreView = (TextView) findViewById( R.id.dealer_score );
+        userScoreView = (TextView) findViewById( R.id.user_score );
+
         // Dealer and User both get two cards
         dealerInitialCard = (ImageView) findViewById( R.id.dealer_card );
         dealerCards.add( mDeck.dealCard() ); // Needs to remain faceDown until user hits stay
 
         // Set limits
-        cardHeight = dealerInitialCard.getHeight();
-        cardWidth = dealerInitialCard.getWidth();
+        myparams =  dealerInitialCard.getLayoutParams();
 
         // Deal and Create a new ImageView Card
         dealerCards.add( mDeck.dealCard() );
         ImageView newCard = new ImageView(this);
         newCard.setImageResource( dealerCards.get(1).getFaceID() );
-        newCard.setMinimumWidth( cardWidth );
-        newCard.setMaxHeight( cardHeight );
 
         //Add the the Relative layout
-        dealerCardGroup.addView( newCard );
+        dealerCardGroup.addView( newCard, myparams );
 
 
+
+        // Set up user Cards
         userInitialCard = (ImageView) findViewById( R.id.user_card );
         userCards.add( mDeck.dealCard() );
-
         userInitialCard.setImageResource( userCards.get(0).getFaceID() ); // Users first Card
 
+        userCards.add( mDeck.dealCard() );
+        ImageView tempCard = new ImageView(this);
+        tempCard.setImageResource( userCards.get(1).getFaceID() ); // Users second Card
+
+        userCardGroup.addView( tempCard, myparams );
+
+        // Calculate user Score
+        userScore = calculateScore( userCards );
+
+        // Update Score Views
+        userScoreView.setText( userScore );
 
     }
 
@@ -105,6 +119,14 @@ public class BlackJack extends Activity
     public int calculateScore(Vector<Card> pVect )
     {
         int retVal = -1; // Default to invalid
+
+        //Loop through vector
+
+        //tally up card values
+
+        //if >10 then just 10
+
+        // if 1 then either 1 or 10
 
         return retVal;
     }
