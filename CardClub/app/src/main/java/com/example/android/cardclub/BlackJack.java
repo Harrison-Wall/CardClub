9ab26fb9b3d2e8b00103ccc84d77fe83b89343ab
Harrison Wall <harrisonwall99@yahoo.com>
2018-12-01 15:52:06 -0500

    Working on adding Cards

--- CardClub/app/src/main/java/com/example/android/cardclub/BlackJack.java
+++ CardClub/app/src/main/java/com/example/android/cardclub/BlackJack.java
@@ -3,18 +3,26 @@ package com.example.android.cardclub;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import java.util.Vector;
 
 public class BlackJack extends Activity
 {
     private Deck mDeck;
-    private int userScore, dealerScore;
+    private int userScore, dealerScore, cardWidth, cardHeight;
     private Vector<Card> userCards, dealerCards;
 
+    private ImageView dealerInitialCard, userInitialCard;
+    private LinearLayout userCardGroup, dealerCardGroup;
+    private int marginOffset;
 
+    private TextView userScoreView, dealerScoreView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -42,6 +50,12 @@ public class BlackJack extends Activity
             }
         });
 
+        marginOffset = 8;
+
+        // Set up the card groups
+        dealerCardGroup = (LinearLayout) findViewById( R.id.dealer_card_group );
+        userCardGroup = (LinearLayout) findViewById( R.id.user_card_group );
+
         mDeck = new Deck(this);
         mDeck.shuffleDeck();
 
@@ -52,6 +66,29 @@ public class BlackJack extends Activity
         dealerCards = new Vector<Card>();
 
         // Dealer and User both get two cards
+        dealerInitialCard = (ImageView) findViewById( R.id.dealer_card );
+        dealerCards.add( mDeck.dealCard() ); // Needs to remain faceDown until user hits stay
+
+        // Set limits
+        cardHeight = dealerInitialCard.getHeight();
+        cardWidth = dealerInitialCard.getWidth();
+
+        // Deal and Create a new ImageView Card
+        dealerCards.add( mDeck.dealCard() );
+        ImageView newCard = new ImageView(this);
+        newCard.setImageResource( dealerCards.get(1).getFaceID() );
+        newCard.setMinimumWidth( cardWidth );
+        newCard.setMaxHeight( cardHeight );
+
+        //Add the the Relative layout
+        dealerCardGroup.addView( newCard );
+
+
+        userInitialCard = (ImageView) findViewById( R.id.user_card );
+        userCards.add( mDeck.dealCard() );
+
+        userInitialCard.setImageResource( userCards.get(0).getFaceID() ); // Users first Card
+
 
     }
 
